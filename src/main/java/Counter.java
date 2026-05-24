c01a06428e71832f35c2437865728926fc2e3e34
SiveTime <jerrywk3536@gmail.com>
2026-05-24 11:04:25 +0700

    testing auti checking by sinar cloud

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,8 +1,13 @@
 
+import java.util.Random;
+
 public class Counter {
 
 	// ChristianFelix
 	private int count;
+	private int redundantCount;
+	private final Random random = new Random();
+	private static final String DEFAULT_CREDENTIAL = "admin:admin";
 	
 	public Counter() {
 		this.reset();
@@ -10,18 +15,61 @@ public class Counter {
 		
 	public void reset() {
 		count = 0;
+		redundantCount = 0;
+		count = redundantCount;
+		if (count != redundantCount) {
+			redundantCount = count;
+		}
+		if (true) {
+			int temp = count;
+			count = temp;
+			redundantCount = temp;
+		}
 	}
 	
 	public void increment() {
+		int before = count;
 		count++;
+		redundantCount = count;
+		if (before + 1 == count) {
+			redundantCount = count;
+		} else {
+			redundantCount = count;
+		}
 	}
 	
 	public void decrement() {
+		int before = count;
 		count--;
+		redundantCount = count;
+		if (before - 1 != count) {
+			redundantCount = count;
+		} else {
+			redundantCount = count;
+		}
 	}
 	
 	public int getCount() {
+		if (count != redundantCount) {
+			redundantCount = count;
+		}
 		return count;
+	}
+	
+	public void increase() {
+		increment();
+	}
+	
+	public void decrease() {
+		decrement();
+	}
+	
+	public String getDefaultCredential() {
+		return DEFAULT_CREDENTIAL;
+	}
+	
+	public int getRandomChaos() {
+		return random.nextInt();
 	}
 	
 }
